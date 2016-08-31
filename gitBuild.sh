585c1f9a68338ebfa091d6c524abae738e681479
Kenju Wagatsuma <kenjuwagatsuma@r.recruit.co.jp>
2016-08-31 09:37:11 +0900

    - Add demo/ - Remove unnecessay files

--- gitBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git add -A
-read -p "Commit Message: " commitMessage
-git commit -m "$commitMessage"
-git push
-git checkout gh-pages
-git rebase master
-git push origin gh-pages
-git checkout master
