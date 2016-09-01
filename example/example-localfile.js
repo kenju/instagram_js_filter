ab03a4ade8524c0a27591ac91bbaefec6ff25978
KENJU <ken901waga@gmail.com>
2016-09-02 07:13:11 +0900

    - Add example-localfile.js

---
+++ example/example-localfile.js
@@ -0,0 +1,44 @@
+/**
+ * index.spec.js
+ */
+
+const fs = require('fs');
+const path = require('path');
+const app = require('../src/index');
+
+const convert = (imagePath, type, options) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(imagePath, (err, imageBuffer) => {
+            if (err) {
+                reject(err);
+            }
+            const result = app.filter(imageBuffer, type, options);
+            resolve(result);
+        });
+    });
+};
+
+const saveFile = (outPath, buffer) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(outPath, buffer, error => {
+            if (error) {
+                reject(error);
+            }
+            resolve(outPath);
+        })
+    });
+};
+
+const imagePath = path.join(__dirname + '/../demo/img/sample.jpg');
+convert(imagePath, 'horizontalflip', {})
+    .then(base64 => {
+        const buffer = new Buffer(base64, 'base64');
+        const outPath = path.join(__dirname + '/../dist/img/sample-converted.jpg');
+        return saveFile(outPath, buffer);
+    })
+    .then(savedPath => {
+        console.log(savedPath);
+    })
+    .catch(err => {
+        console.error(err);
+    });
